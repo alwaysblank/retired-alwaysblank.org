6bde561d277c3ead3bad1df0e1460c85837d6182
Ben Martinez-Bateman <ben@alwaysblank.org>
2018-01-28 09:32:00 -0800

    Process code  prop w/ markdown.

--- src/components/blocks/code.js
+++ src/components/blocks/code.js
@@ -12,16 +12,21 @@ require(`prismjs/components/prism-yaml.js`);
 require(`prismjs/components/prism-bash.js`);
 require(`prismjs/themes/prism-okaidia.css`);
 import PrismCode from 'react-prism'
+import {Parser, HtmlRenderer} from "commonmark"
 
 class CodeContentBlock extends React.Component {
   render() {
     const block = this.props.content
+    const reader = new Parser()
+    const writer = new HtmlRenderer()
+    const explain = block.explain ? {__html: writer.render(reader.parse(block.explain))} : false
 
     return (
       <div className={`contentBlock--code language-${block.language}`}>
-        {block.explain &&
-          <p className="contentBlock--code__explain">
-            {block.explain}
+        {explain &&
+          <p 
+            dangerouslySetInnerHTML={explain} 
+            className="contentBlock--code__explain">
           </p>
         }
         <pre>
