5f2ea7fb5cc78c502f93e3ed67aebe9928bbe043
Ben (Hypatia) <ben@alwaysblank.org>
2018-01-28 21:27:34 -0800

    Fixed title.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,23 +8,23 @@ import './index.css'
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: `0 auto`,
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: `1.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: `white`,
+            textDecoration: `none`,
           }}
         >
           Gatsby
@@ -37,18 +37,18 @@ const Header = () => (
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Always Blank"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: `description`, content: `Web development for the future.` },
+        { name: `keywords`, content: `web, development, coding, php, javascript, wordpress` },
       ]}
     />
     <Header />
     <div
       style={{
-        margin: '0 auto',
+        margin: `0 auto`,
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
+        padding: `0px 1.0875rem 1.45rem`,
         paddingTop: 0,
       }}
     >
